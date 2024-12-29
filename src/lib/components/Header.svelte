5113c1a7b01518662cdd854696845d1bf802143b
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2024-12-29 18:57:59 +0100

    content update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <header>
 	<a href="/"><h1>Austrian Web Dev Map ⛰️ 🇦🇹</h1></a>
-	<p>Searching for a job can be tiresome, this map might help (a bit). Good luck!</p>
+	<p>Looking for a job can be tiresome, this map might help (a bit). Good luck!</p>
 </header>
 
 <a
